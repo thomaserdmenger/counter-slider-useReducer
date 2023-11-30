2245e9e625599e589447083c4c887670cf5f3d30
thomaserdmenger <thomas.erdmenger@googlemail.com>
2023-11-30 10:14:33 +0100

    Add step state to slider component and styles

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,9 +2,17 @@ import * as React from 'react'
 import styles from '../App.module.css'
 
 export default function Slider() {
+  const [value, setValue] = React.useState(1)
+
+  const handleChange = e => {
+    const value = Number(e.target.value)
+    setValue(value)
+  }
+
   return (
-    <div>
-      <input type='range' />
+    <div className={styles.sliderContainer}>
+      <input type='range' value={value} onChange={handleChange} />
+      <span>{value}</span>
     </div>
   )
 }
