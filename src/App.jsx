8df8eb2ad2fa0f29cca9da25582f86dcd6776ae8
thomaserdmenger <thomas.erdmenger@googlemail.com>
2023-11-30 09:27:38 +0100

    Update states with useReducer: increment, decrement, reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
 import * as React from 'react'
 import styles from './App.module.css'
 
+const initialValue = { count: 0 }
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'decrement': {
+      return {
+        count: state.count - 1,
+      }
+    }
+    case 'increment': {
+      return {
+        count: state.count + 1,
+      }
+    }
+    case 'reset': {
+      return {
+        count: 0,
+      }
+    }
+    default: {
+      throw new Error(
+        `Action of the following type is not available: ${action.type}`
+      )
+    }
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = React.useReducer(reducer, initialValue)
+
+  const handleDecrement = () => dispatch({ type: 'decrement' })
+  const handleIncrement = () => dispatch({ type: 'increment' })
+  const handleReset = () => dispatch({ type: 'reset' })
+
   return (
     <>
-      <h1>0</h1>
+      <h1>{state.count}</h1>
       <div className={styles.buttonContainer}>
-        <button>-</button>
-        <button>+</button>
-        <button>0</button>
+        <button onClick={handleDecrement}>-</button>
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleReset}>0</button>
       </div>
     </>
   )
