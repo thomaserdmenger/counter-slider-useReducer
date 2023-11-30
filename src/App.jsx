e30d96f65fd7f2bd80cbd3a1f10238d4457834ce
thomaserdmenger <thomas.erdmenger@googlemail.com>
2023-11-30 09:17:10 +0100

    Add basic structure and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import * as React from 'react'
+import styles from './App.module.css'
+
 export default function App() {
-  return null
+  return (
+    <>
+      <h1>0</h1>
+      <div className={styles.buttonContainer}>
+        <button>-</button>
+        <button>+</button>
+        <button>0</button>
+      </div>
+    </>
+  )
 }
